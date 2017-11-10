f4082e10a459ca0a5ca5c35e376458e39c583689
Ben <benjosua@live.de>
2017-11-10 14:08:03 +0100

    Update src.ino

--- src/src.ino
+++ src/src.ino
@@ -1,44 +1,28 @@
-
 /* Line 6 Pod Controller
  *
  *
  * CODE BY BEN
  */
 
+/* Prototyps  */
+void sendtopod(int MidiCommand, int MidiController, int MidiValue);
+int readanalog(int AnalogPin);
+/* Vars ----- */
 
 
-
-/* */
-/* Status-Byte     Data-Byte     Data-Byte   */
-/* Midi-Command    Controller    Value 0-127 */
-/* 1 011 0000      00000000      00000000    */
-/*    |   '- Channel                         */
-/*    '- Controlchange-Command               */
-/* Program Change on Channel 1          176  */
-/* Control Change on Channel 1          192  */
-/* Program Change   1    0 - 127             */
-/* Stomp            25   0 / 127             */
-/* Mod              50   0 / 127             */
-/* Delay            28   0 / 127             */
-
+/* Setup ---- */
 void setup() {
-  Serial.begin(31250); 
+  Serial.begin(31250);
 }
 
-
-
+/* Loop ----- */
 void loop(){
 
 
 for(int i=0;i<65;i++)
 {
-delay(1000);
-Serial.write(192);
-
-
-
-Serial.write(1);
-Serial.write(i);
+  delay(1000);
+  sendtopod(192,1,i);
 }
 
 
@@ -46,4 +30,15 @@ Serial.write(i);
 
 
 
+}
+/* Functinos  */
+void sendtopod(int MidiCommand, int MidiController, int MidiValue){
+  Serial.write(MidiCommand);
+  Serial.write(MidiController);
+  Serial.wirte(MidiValue);
+}
+int readanalog(int AnalogPin){
+  int value = 0;  
+  value = analogRead(AnalogPin);
+  return valule / 4; 
 }
